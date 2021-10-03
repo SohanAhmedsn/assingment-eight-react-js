d9c7d94695f899b1d84ab4b71ab7dad8dac33dd9
sn77sohan <sohan.ahmedself@gmail.com>
2021-10-03 15:35:05 +0600

    seond-edition

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,9 @@ const Header = () => {
     // show header part
     return (
         <div className='bg-secondary text-white pt-2 pb-2 mb-4'>
-            <h2>Make an Cherity Concert</h2>
-            <p>Here you choose your singer for concert (free or Hired)</p>
-            <h2>Total Budget: 100000 tk</h2>
+            <h2>Charity Team</h2>
+            <p>You Now Hire the team</p>
+            <h2>Total Budget 100M</h2>
         </div>
     );
 };
